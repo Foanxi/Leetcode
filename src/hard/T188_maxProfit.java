de30e512958a6fd3cc405540482aeac5027e1b66
foanxi <zhihuiyuyouyi@foxmail.com>
2023-03-26 19:42:43 +0800

    feat:
    新增题目

--- src/hard/T188_maxProfit.java
+++ src/hard/T188_maxProfit.java
@@ -5,16 +5,16 @@ public class T188_maxProfit {
         // dp[i]表示第j种状态下所剩下的最大现金
         // j为奇数表示持有股票
         // j为偶数表示卖出股票
-        int[] dp = new int[k*2+1];
-        for (int j=1;j<=2*k;j+=2){
+        int[] dp = new int[k * 2 + 1];
+        for (int j = 1; j <= 2 * k; j += 2) {
             dp[j] -= prices[0];
         }
         for (int i = 1; i < prices.length; i++) {
-            for (int j=1;j<=k*2;j++){
-                int sym = j%2 == 0?1:-1;
-                dp[j] = Math.max(dp[j],dp[j-1] + prices[i] * sym);
+            for (int j = 1; j <= k * 2; j++) {
+                int sym = j % 2 == 0 ? 1 : -1;
+                dp[j] = Math.max(dp[j], dp[j - 1] + prices[i] * sym);
             }
         }
-        return dp[2*k];
+        return dp[2 * k];
     }
 }
