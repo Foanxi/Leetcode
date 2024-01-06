143510e9766c447304ae4672b0705349396d8983
foanxi <zhihuiyuyouyi@foxmail.com>
2024-01-07 00:02:32 +0800

    feature: 新增题目

--- src/easy/T383_canConstruct.java
+++ src/easy/T383_canConstruct.java
@@ -3,21 +3,15 @@ package easy;
 public class T383_canConstruct {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] word = new int[26];
-        for (char c:magazine.toCharArray()) {
-            word[c-'a']++;
+        for (char c : magazine.toCharArray()) {
+            word[c - 'a']++;
         }
-        for (char c:ransomNote.toCharArray()){
-            word[c-'a']--;
+        for (char c : ransomNote.toCharArray()) {
+            word[c - 'a']--;
         }
-        for (int t:word) {
-            if (t<0)return false;
+        for (int t : word) {
+            if (t < 0) return false;
         }
         return true;
-    }
-
-    public static void main(String[] args) {
-        String ransonNote="aa";
-        String magzine="ab";
-        System.out.println(new T383_canConstruct().canConstruct(ransonNote, magzine));
     }
 }
