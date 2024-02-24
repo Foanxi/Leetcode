7db6fe853d0e31a240e6007c2e0e57810bae68d3
foanxi <zhihuiyuyouyi@foxmail.com>
2024-02-24 10:12:05 +0800

    feature: 新增题目

--- src/middle/T2583_kthLargestLevelSum.java
+++ src/middle/T2583_kthLargestLevelSum.java
@@ -11,7 +11,9 @@ public class T2583_kthLargestLevelSum {
         if (root == null) {
             return 0;
         }
-        Queue<Long> max = new PriorityQueue<>(k);
+        // 对于比int类型精度更大的类型，只能使用该办法,使用16行的方式会出现错误
+        // Queue<Long> max = new PriorityQueue<>((o1, o2) -> (int) (o2 - o1));
+        Queue<Long> max = new PriorityQueue<>((o1, o2) -> o2.compareTo(o1));
         Queue<TreeNode> queue = new ArrayDeque<>();
         queue.add(root);
         while (!queue.isEmpty()) {
@@ -27,17 +29,13 @@ public class T2583_kthLargestLevelSum {
                     queue.add(poll.right);
                 }
             }
-            if (max.size() < k) {
-                max.add(sum);
-            } else {
-                if (max.peek() < sum) {
-                    max.poll();
-                    max.add(sum);
-                }
-            }
+            max.add(sum);
         }
         if (k > max.size()) {
             return -1;
+        }
+        for (int i = 0; i < k - 1; i++) {
+            max.poll();
         }
         return max.poll();
     }
